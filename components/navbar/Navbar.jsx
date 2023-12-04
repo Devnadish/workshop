afa6b27d3494552d0413724ff8a1cc15478cd1b0
devnadish <devnadish@gmail.com>
2023-12-05 01:14:54 +0300

    v1

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import BuregerMenu from "./BurgerMenu";
 
 function Navbar() {
   return (
-    <header className="sticky top-0 flex shadow-md max-w-6xl w-full items-center justify-between  text-sm   bg-orange-500  h-16 border px-4  z-50">
-      <div className="flex  items-center gap-2">
+    <header className="sticky top-0 flex shadow-md max-w-6xl w-full items-center justify-between  text-sm   bg-orange-500  h-16 border px-2  z-50">
+      <div className="flex  items-center gap-0">
         <BuregerMenu />
         <div className="relative ">
           <p className="absolute -top-2 -left-2  flex items-center justify-center rounded-full bg-green-700 text-white text-tiny  h-5 w-5">
@@ -15,7 +15,7 @@ function Navbar() {
           <BellRing className="text-white" />
         </div>
       </div>
-      <p className="text-md text-primary-foreground font-bold  py-1 border-red-500 ">
+      <p className="text-xl text-primary-foreground font-bold  py-1 border-red-500 ">
         الصحفي لصيانة السيارات
       </p>
       <Link href={"/"}>
